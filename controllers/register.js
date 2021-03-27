04c121a493026978439f6e532ec6fe5151a389d9
Dan Cotti <dancotti.elf@gmail.com>
2021-03-27 13:54:03 +0000

    debugging

--- controllers/register.js
+++ controllers/register.js
@@ -27,7 +27,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json(err))
 }
 
 module.exports = {
