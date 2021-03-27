9a68821a8e34924104adf206d3b823e1ff8603e0
Dan Cotti <dancotti.elf@gmail.com>
2021-03-27 13:41:34 +0000

    knex update

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-        // user: 'postgres',
-        // password: 'test',
-        // database: 'facerecognition'
-    }
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 const app = express();
