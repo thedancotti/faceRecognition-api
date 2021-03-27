1b437c9246206af57117b8c9970a95e9384e2a8f
Dan Cotti <dancotti.elf@gmail.com>
2021-03-27 13:35:19 +0000

    connected db to heroku

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'facerecognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+        // user: 'postgres',
+        // password: 'test',
+        // database: 'facerecognition'
     }
 });
 
